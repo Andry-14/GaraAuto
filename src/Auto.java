3b89717fe7ed2b55a9c461855d4181ee885dd8a2
juliklatat <giulio.gamboni2007@gmail.com>
2025-11-11 13:37:49 +0100

    aggiustamento

--- src/Auto.java
+++ src/Auto.java
@@ -1,10 +1,33 @@
+import java.util.ArrayList;
+
 public class Auto implements Runnable {
-    String pilota;
 
+    private int numero;
+    private double velocita;
+    private double distanzaPercorsa;
+    private boolean inBox;
+    private Giudice podio;
+    private ArrayList<Auto> autoList;
+    private double distanzaGara;
 
-    public void run(){
-
+    public void avviaGara() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
     }
+
+    public void attendiFineGara() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    public void mostraPodio() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
+    public void pitStop() {
+        throw new UnsupportedOperationException("The method is not implemented yet.");
+    }
+
 }
-
-
---
+++ src/Giudice.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Giudice {
+    private int vincitore;
+    private ArrayList<String> podio;
+
+    public ArrayList<String> getClassifica(){
+
+        return null;
+    }
+
+    public void verifica(){
+
+    }
+
+
+
+}
